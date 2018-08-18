74c17ef937b8fc8b1f7d85732cbb4488ce32a95c
w3cj <cj@null.computer>
2018-08-18 14:59:34 -0400

    :sparkles: Add Player Select Screen

--- opoly/src/App.js
+++ opoly/src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 import PlayerSelect from './containers/PlayerSelect';
 
 class App extends Component {
+  state = {
+    players: [],
+    gameStarted: false
+  };
+
+  startGame = (players) => {
+    console.log(players);
+  }
+
   render() {
     return (
       <div className="App">
-        <PlayerSelect />
+        <PlayerSelect startGame={this.startGame} />
       </div>
     );
   }
--- opoly/src/containers/PlayerSelect.js
+++ opoly/src/containers/PlayerSelect.js
@@ -4,19 +4,62 @@ class PlayerSelect extends Component {
   state = {
     pawns: ['angular', 'js', 'react', 'vue'],
     currentPlayerSelect: 1,
-    players: []
+    players: [],
+    readyToStart: false
   };
+
+  setPlayer = (pawn) => {
+    this.setState((prevState) => ({
+      currentPlayerSelect: prevState.currentPlayerSelect + 1,
+      players: [
+        ...prevState.players, {
+          number: prevState.currentPlayerSelect,
+          pawn
+        }
+      ],
+      readyToStart: prevState.currentPlayerSelect === 2 ? true : false
+    }));
+  }
 
   render() {
     return (
       <div>
-        <h1>Player {this.state.currentPlayerSelect}, select a pawn:</h1>
+        <div className="players">
+          {
+            this.state.players.map(player => (
+              <div key={player.number} className="player">
+                <p>Player {player.number}</p>
+                <img
+                  className="player-pawn"
+                  alt={player.pawn}
+                  src={`./pawns/${player.pawn}-pawn.png`} />
+              </div>
+            ))
+          }
+        </div>
         {
-          this.state.pawns.map(pawn => (
-            <img
-              className="pawn"
-              src={`./pawns/${pawn}-pawn.png`} />
-          ))
+          this.state.readyToStart ?
+          <React.Fragment>
+            <h1>Click start to begin!</h1>
+            <button className="start-button">Start</button>
+          </React.Fragment> :
+          <React.Fragment>
+            <h1>Player {this.state.currentPlayerSelect}, select a pawn:</h1>
+            {
+              this.state.pawns.map(pawn => (
+                <div className="pawn-container" key={pawn} onClick={() => this.setPlayer(pawn)}>
+                  <img
+                    className="pawn"
+                    alt={pawn}
+                    src={`./pawns/${pawn}-pawn.png`} />
+                  <img
+                    className="pawn-logo"
+                    alt={pawn}
+                    src={`./logos/${pawn}.png`} />
+                </div>
+              ))
+            }
+          </React.Fragment>
         }
       </div>
     );
